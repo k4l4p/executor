df226c9e8b8203025c24eb3da94ceacf7d5e20cd
k4l4p <kalapwong@gmail.com>
2024-05-14 09:30:55 +0800

    chore: update rpc

--- app/karak.ts
+++ app/karak.ts
@@ -1,37 +1,33 @@
-import { Chain } from 'wagmi'
+import { Chain } from "wagmi"
 
 export const karak = {
-  id: 2410,
-  name: 'Karak Mainnet',
-  network: 'karak',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'ETH',
-    symbol: 'ETH',
-  },
-  rpcUrls: {
-    public: {
-      http: [
-        'https://partner.rpc.karak.network/?key=airpuff-lGpTnlXB56D3Vqc8ekod/',
-      ],
-    },
-    default: {
-      http: [
-        'https://partner.rpc.karak.network/?key=airpuff-lGpTnlXB56D3Vqc8ekod/',
-      ],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Mode explorer',
-      url: 'https://explorer.karak.network/',
-    },
-    default: { name: 'Mode explorer', url: 'https://explorer.karak.network/' },
-  },
-  contracts: {
-    // multicall3: {
-    //   address: '0xca11bde05977b3631167028862be2a173976ca11',
-    //   blockCreated: 11_907_934,
-    // },
-  },
+	id: 2410,
+	name: "Karak Mainnet",
+	network: "karak",
+	nativeCurrency: {
+		decimals: 18,
+		name: "ETH",
+		symbol: "ETH",
+	},
+	rpcUrls: {
+		public: {
+			http: ["https://rpc.karak.network/"],
+		},
+		default: {
+			http: ["https://rpc.karak.network/"],
+		},
+	},
+	blockExplorers: {
+		etherscan: {
+			name: "Mode explorer",
+			url: "https://explorer.karak.network/",
+		},
+		default: { name: "Mode explorer", url: "https://explorer.karak.network/" },
+	},
+	contracts: {
+		// multicall3: {
+		//   address: '0xca11bde05977b3631167028862be2a173976ca11',
+		//   blockCreated: 11_907_934,
+		// },
+	},
 } as const satisfies Chain
